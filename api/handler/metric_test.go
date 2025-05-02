e57366eea149453110a690a907ceb34bb6d7b1bc
John Mayou <john@johnmayou.com>
2025-05-01 23:06:47 -0500

    use lower case struct keys for tests

--- api/handler/metric_test.go
+++ api/handler/metric_test.go
@@ -14,18 +14,18 @@ import (
 
 func TestTrackMetric(t *testing.T) {
 	tests := map[string]struct {
-		Body   handler.TrackMetricBody
-		Assert func(t *testing.T, h *handler.Handler)
+		body   handler.TrackMetricBody
+		assert func(t *testing.T, h *handler.Handler)
 	}{
 		"problem_change_total increments": {
-			Body: handler.TrackMetricBody{Name: "problem_change_total", Labels: map[string]int{"problem_number": 1}},
-			Assert: func(t *testing.T, h *handler.Handler) {
+			body: handler.TrackMetricBody{Name: "problem_change_total", Labels: map[string]int{"problem_number": 1}},
+			assert: func(t *testing.T, h *handler.Handler) {
 				require.Equal(t, 1.0, testutil.ToFloat64(h.Metrics.ProblemChangeCounter.WithLabelValues("1")))
 			},
 		},
 		"terminal_focus_total increments": {
-			Body: handler.TrackMetricBody{Name: "terminal_focus_total", Labels: map[string]int{"problem_number": 1}},
-			Assert: func(t *testing.T, h *handler.Handler) {
+			body: handler.TrackMetricBody{Name: "terminal_focus_total", Labels: map[string]int{"problem_number": 1}},
+			assert: func(t *testing.T, h *handler.Handler) {
 				require.Equal(t, 1.0, testutil.ToFloat64(h.Metrics.TerminalFocusCounter.WithLabelValues("1")))
 			},
 		},
@@ -33,7 +33,7 @@ func TestTrackMetric(t *testing.T) {
 
 	for tcName, tc := range tests {
 		t.Run(tcName, func(t *testing.T) {
-			jsonBody, err := json.Marshal(tc.Body)
+			jsonBody, err := json.Marshal(tc.body)
 			require.NoError(t, err)
 
 			req := httptest.NewRequest(http.MethodPost, "/metrics/track", bytes.NewReader(jsonBody))
@@ -42,7 +42,7 @@ func TestTrackMetric(t *testing.T) {
 			handler := handler.NewTestHandler(nil)
 			handler.TrackMetric(w, req)
 
-			tc.Assert(t, handler)
+			tc.assert(t, handler)
 		})
 	}
 }
