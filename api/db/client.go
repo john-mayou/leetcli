72dc14de27e1041bda10c6136fc8ba4ccd57935c
John Mayou <john@johnmayou.com>
2025-05-01 20:42:53 -0500

    add problem and problem submission functions to interface

--- api/db/client.go
+++ api/db/client.go
@@ -12,6 +12,18 @@ type DBClient interface {
 	FindUserByGithubID(id string) (*model.User, error)
 	UpdateUser(user *model.User) error
 	DeleteUser(id string) error
+
+	// problem
+	CreateProblem(problem *model.Problem) (*model.Problem, error)
+	FindProblemByID(id string) (*model.Problem, error)
+	UpdateProblem(problem *model.Problem) error
+	DeleteProblem(is string) error
+
+	// problem submission
+	CreateProblemSubmission(ps *model.ProblemSubmission) (*model.ProblemSubmission, error)
+	FindProblemSubmissionByID(id string) (*model.ProblemSubmission, error)
+	UpdateProblemSubmission(ps *model.ProblemSubmission) error
+	DeleteProblemSubmission(id string) error
 }
 
 type Client struct {
