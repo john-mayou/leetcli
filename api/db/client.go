78b2d6a3bf9174f34c83756be2980eb8a18bbe8e
John Mayou <john@johnmayou.com>
2025-05-04 02:33:09 -0500

    add new db methods to client struct

--- api/db/client.go
+++ api/db/client.go
@@ -12,18 +12,22 @@ type DBClient interface {
 	FindUserByGithubID(id string) (*model.User, error)
 	UpdateUser(user *model.User) error
 	DeleteUser(id string) error
+	ListUsers() ([]*model.User, error)
 
 	// problem
 	CreateProblem(problem *model.Problem) (*model.Problem, error)
 	FindProblemByID(id string) (*model.Problem, error)
+	FindProblemBySlug(slug string) (*model.Problem, error)
 	UpdateProblem(problem *model.Problem) error
 	DeleteProblem(is string) error
+	ListProblems() ([]*model.Problem, error)
 
 	// problem submission
 	CreateProblemSubmission(ps *model.ProblemSubmission) (*model.ProblemSubmission, error)
 	FindProblemSubmissionByID(id string) (*model.ProblemSubmission, error)
 	UpdateProblemSubmission(ps *model.ProblemSubmission) error
 	DeleteProblemSubmission(id string) error
+	ListProblemSubmissions() ([]*model.ProblemSubmission, error)
 }
 
 type Client struct {
